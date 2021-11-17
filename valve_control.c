77a31377591543ecf992aaf0f1803c7e65c4c9bf
Wonyoung <wonyoungjjang@gmail.com>
2021-11-17 14:59:56 +0900

    do_valve_control

--- valve_control.c
+++ valve_control.c
@@ -4,16 +4,28 @@ typedef long long TIME_T;
 #define ONE_SECOND      (50000000)
 #define PRINT_INTERVAL  (ONE_SECOND/2)
 
+#define OPEN_VALVE_STEP     (1)
+#define CLOSE_VALVE_STEP    (2)
+
 void open_valve(void);
 void close_valve(void);
 
+void do_valve_control(void);
+
 int main(void)
 {
-    for (TIME_T t = 0; t < ONE_SECOND; t++)
-        open_valve();
+    while(1)
+    {
+        do_valve_control();
 
-    for (TIME_T t = 0; t < ONE_SECOND*5; t++)
-        close_valve();
+        // do_motor_control();
+        // do_pump_control();
+
+        // do_led_control();
+
+        // do_something_else1();
+        // do_something_else2();
+    }
 
     return 0;
 }
@@ -34,4 +46,21 @@ void close_valve(void)
     static long long cnt = 0;
     if (++cnt % PRINT_INTERVAL == 0)
         printf("Valve - Close #%lld\n", cnt / PRINT_INTERVAL);
+}
+
+int gState = OPEN_VALVE_STEP;
+
+void do_valve_control(void)
+{
+    switch(gState)
+    {
+        case OPEN_VALVE_STEP:
+            open_valve();
+            break;
+        case CLOSE_VALVE_STEP:
+            close_valve();
+            break;
+        default:
+            break;
+    }
 }
